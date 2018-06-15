915705d99d91395afc81af84d55278388069a73a
LadyFromMars <40034405+LadyFromMars@users.noreply.github.com>
2018-06-14 21:38:55 -0400

    the file was changed by mistake, here is the original

--- text_upgrade.js
+++ text_upgrade.js
@@ -1,69 +1,44 @@
-const getUserChoice = userInput =>{
-userInput = userInput.toLowerCase();
- if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
-   return userInput;
- } else {
-   console.log ('Error: incorrect input');
- }}
+let overusedWords = ['really', 'very', 'basically'];
 
+let unnecessaryWords = ['extremely', 'literally', 'actually' ];
 
+let storyWords = story.split(' ');
 
-const getComputerChoice =() =>{
- randomNumber = Math.floor(Math.random() *3);
+let betterWords = storyWords.filter(word => ! overusedWords.includes(word));
+
+//Better words
+
+let really = 0;
+let very = 0;
+let basically = 0;
+
+for (let swIndex = 0; swIndex <= storyWords.length; swIndex++) {
+    bword = storyWords[swIndex];
+
+if (bword ==='really'){
+  really++
+} else if (bword  ==='very'){
+  very++
+} else if (bword ==='basically') {
+  basically++
+};
+if( (! really>1 && bword==='really') || 
+   (! very > 1 && bword==='very') ||
+   (! basically > 1 && bword==='basically')) {
+  betterWords.pop (bword);
+} 
   
-  switch (randomNumber) {
-    case 0:
-      return 'rock';
-    case 1:
-      return 'scissors'; 
-    case 2: 
-      return 'paper';
+};
+console.log(very, basically, really);
+betWords = betterWords.join(' ');
+console.log(betWords);
+
+//Counting sentences
+
+let sentences = 0;
+betterWords.forEach( word => {
+  if (word[word.length-1] === '.' || word[word.length-1] === '!') {
+    sentences+=1;
   }
-}
-
-
-const determineWinner = (userChoice, computerChoice) => {
-  
-  if (userChoice === computerChoice){
-    return 'It is a tie!';} 
-  
-   else if (userChoice === 'rock') {
-        if (computerChoice === 'paper'){
-      	return 'Computer brain is a gain!';} 
-        else {return 'Go Humans!!';}}
-     
-   if (userChoice  === 'paper'){
-    if (computerChoice === 'scissors'){
-      return 'You snooze you lose!';
-    } else {
-      return 'You are rock! ';
-    }
-  } ;
-  if (userChoice === 'scissors'){
-      if (computerChoice === 'rock'){
-        return 'Dont be upsetti eat some spaghetti!';
-      } 
-    else {
-        return 'Whooohooo! Eat my dust!';
-      } 
-  } 
-}
-
-const playGame = () => {
-  
-  const userChoice = getUserChoice('rock'); 
-  const computerChoice = getComputerChoice();
-  
-  console.log(`You chose a ${userChoice}`);
-  console.log(`AI chose a  ${computerChoice}`);
-  
-  console.log(determineWinner(userChoice, computerChoice));
-  
-}
-
-playGame();
-
-
-
-
-
+});
+console.log('We have ' + sentences + ' sentences');
